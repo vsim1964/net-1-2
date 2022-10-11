37f502befb6c619eb5a6aeb58dd9151f00b69fcc
vsim1964 <vsim1964@gmail.com>
2022-10-11 22:42:24 +0300

    Start

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+// import ShopItemFunc from './Components/ShopItemFunc/ShopItemFunc';
+import ShopItemClass from './Components/ShopItemClass/ShopItemClass';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+};
+
+function App() {
+  return (
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="window">
+          {/*<ShopItemFunc item={item}/>*/}
+          <ShopItemClass item={item}/>
+        </div>
+      </div>
+  );
+}
+
+export default App;
+
+item.propTypes = {
+  brand: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  descriptionFull: PropTypes.string,
+  price: PropTypes.number,
+  currency: PropTypes.string
+};
